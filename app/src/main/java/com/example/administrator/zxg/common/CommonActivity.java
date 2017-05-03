cc0183059b7cfa5c78db65a4ac89d96cf3da77a1
zhaoxg <zhaoxg@yaguit.com>
2017-05-03 15:29:26 +0800

    #20170503 commit

--- app/src/main/java/com/example/administrator/zxg/common/CommonActivity.java
+++ app/src/main/java/com/example/administrator/zxg/common/CommonActivity.java
@@ -2,6 +2,7 @@ package com.example.administrator.zxg.common;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by Administrator on 2016/10/5.
@@ -10,5 +11,11 @@ public class CommonActivity  extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    public  void  onBackPressed(View view){
+
+        finish();
+
     }
 }
