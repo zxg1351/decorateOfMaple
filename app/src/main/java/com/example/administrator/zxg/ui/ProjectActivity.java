af1239307401965878600a352dbdceffa5e21139
zhaoxg <zhaoxg@yaguit.com>
2017-05-16 17:26:14 +0800

    #20170516 commit

--- app/src/main/java/com/example/administrator/zxg/ui/ProjectActivity.java
+++ app/src/main/java/com/example/administrator/zxg/ui/ProjectActivity.java
@@ -1,7 +1,12 @@
 package com.example.administrator.zxg.ui;
 
+import com.example.administrator.zxg.common.CommonActivity;
+
 /**
  * Created by Administrator on 2016/10/22.
  */
-public class ProjectActivity {
+public class ProjectActivity extends CommonActivity{
+
+
+
 }
